fe05b01a8f3f4f1c841ef6a651ac7ae840425526
Dipen Ved <vedipen@gmail.com>
2017-07-06 20:01:57 +0530

    Add functions to controllers, add complete methods

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -7,13 +8,17 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
       flash[:sucess] = "Todo List item was deleted"
     else
       flash[:error] = "Todo List item could not be deleted"
     end
     redirect_to @todo_list
+  end
+
+  def complete
+    @todo_item.toggle_completion
+    redirect_to @todo_list, notice: "Todo item completed"
   end
 
   private
@@ -25,4 +30,7 @@ class TodoItemsController < ApplicationController
       params[:todo_item].permit(:content)
     end
 
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
 end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,16 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+
+  def completed?
+    !completed_at.blank?
+  end
+
+  def toggle_completion
+    if completed?
+      update_attribute(:completed_at, nil)
+    else
+      update_attribute(:completed_at, Time.now)
+    end
+  end
+
 end
